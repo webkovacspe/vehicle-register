2790568d310f98990a782bca2efacd879625cf15
webkovacs <webkovacs@gmail.com>
2023-01-21 11:49:08 +0100

    félállapotos

--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleregister/VehicleEntity.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleregister/VehicleEntity.java
@@ -1,9 +1,9 @@
 package hu.kovacspeterzoltan.bootcamp.vehicleregister;
 
 public class VehicleEntity {
+    public String registrationNumber;
     public String vehicleRegister;
     public String vehicle;
-    public String registrationNumber;
     public String make;
     public String model;
     public int numberOfSeats;
--- src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleregister/VehicleStorageInterface.java
+++ src/main/java/hu/kovacspeterzoltan/bootcamp/vehicleregister/VehicleStorageInterface.java
@@ -2,6 +2,5 @@ package hu.kovacspeterzoltan.bootcamp.vehicleregister;
 
 public interface VehicleStorageInterface {
     void saveVehicle(VehicleEntity vehicle);
-
     VehicleEntity getVehicle(String registrationNumber);
 }
